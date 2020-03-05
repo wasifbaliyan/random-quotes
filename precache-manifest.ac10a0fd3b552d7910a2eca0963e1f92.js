fabca0c89ceb34b46e4f35181e50e9fdd491fd93
Wasif Baliyan <w.baliyan@gmail.com>
2020-03-05 20:41:21 +0530

    Updates

---
+++ precache-manifest.ac10a0fd3b552d7910a2eca0963e1f92.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04990e55320d30e256bbc2137bd2096e",
+    "url": "/random-quotes/index.html"
+  },
+  {
+    "revision": "a84341cf25a77ea1b4ba",
+    "url": "/random-quotes/static/css/2.54ddc2c7.chunk.css"
+  },
+  {
+    "revision": "9de12cfe3b9b87cb57f7",
+    "url": "/random-quotes/static/css/main.69d57d29.chunk.css"
+  },
+  {
+    "revision": "a84341cf25a77ea1b4ba",
+    "url": "/random-quotes/static/js/2.c6b2238c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/random-quotes/static/js/2.c6b2238c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de12cfe3b9b87cb57f7",
+    "url": "/random-quotes/static/js/main.475a0180.chunk.js"
+  },
+  {
+    "revision": "942ce24578d96d107747",
+    "url": "/random-quotes/static/js/runtime-main.92b3bab1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/random-quotes/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/random-quotes/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/random-quotes/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/random-quotes/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/random-quotes/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
